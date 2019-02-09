186dd6a59699112904c3fec63655ec2919dab05b
pyromani4 <sedonaarizona9@gmail.com>
2019-02-09 13:13:28 -0500

    Initial Commit

---
+++ jsontest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jsontest
+//
+//  Created by RS on 2019/02/09.
+//  Copyright © 2019 RS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ jsontestTests/jsontestTests.swift
@@ -0,0 +1,36 @@
+//
+//  jsontestTests.swift
+//  jsontestTests
+//
+//  Created by RS on 2019/02/09.
+//  Copyright © 2019 RS. All rights reserved.
+//
+
+import XCTest
+@testable import jsontest
+
+class jsontestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
